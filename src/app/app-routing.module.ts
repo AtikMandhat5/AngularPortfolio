a6f17896bc452209d724c037a496d082cc685abb
Atik Mandhat <atikmandhat86@gmail.com>
2022-03-07 22:08:20 +0530

    all commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResultsComponent } from './results/results.component';
+import { SkillsComponent } from './skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"results",component:ResultsComponent},
+  {path:"skills",component:SkillsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalComponent } from './personal/personal.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ResultsComponent } from './results/results.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalComponent,
+    SkillsComponent,
+    ResultsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
